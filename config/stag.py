2caee1d674eb55723a79e6698468a21c9b6d3720
xogas <askxogas@gmail.com>
2025-03-26 22:06:03 +0800

    修改数据库配置到stag

--- config/stag.py
+++ config/stag.py
@@ -10,16 +10,17 @@ Unless required by applicable law or agreed to in writing, software distributed 
 an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
+
 from config import RUN_VER
 from config.default import FRONTEND_BACKEND_SEPARATION
 
-if RUN_VER == "open":
+if RUN_VER == 'open':
     from blueapps.patch.settings_open_saas import *  # noqa
 else:
     from blueapps.patch.settings_paas_services import *  # noqa
 
 # 预发布环境
-RUN_MODE = "STAGING"
+RUN_MODE = 'STAGING'
 
 # 只对预发布环境日志级别进行配置，可以在这里修改
 # from blueapps.conf.log import set_log_level # noqa
@@ -27,23 +28,21 @@ RUN_MODE = "STAGING"
 # LOGGING = set_log_level(locals())
 
 # 预发布环境数据库可以在这里配置
-# DATABASES.update(
-#     {
-#         'default': {
-#             'ENGINE': 'django.db.backends.mysql',
-#             'NAME': '',  # 外部数据库名
-#             'USER': '',  # 外部数据库用户
-#             'PASSWORD': '',  # 外部数据库密码
-#             'HOST': '',  # 外部数据库主机
-#             'PORT': '',  # 外部数据库端口
-#         },
-#     }
-# )
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'xogas001',  # noqa
+        'USER': 'root',
+        'PASSWORD': '123456789',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    },
+}
 
 # 前后端开发模式下支持跨域配置
 if FRONTEND_BACKEND_SEPARATION:
-    INSTALLED_APPS += ("corsheaders",)
+    INSTALLED_APPS += ('corsheaders',)
     # 该跨域中间件需要放在前面
-    MIDDLEWARE = ("corsheaders.middleware.CorsMiddleware",) + MIDDLEWARE
+    MIDDLEWARE = ('corsheaders.middleware.CorsMiddleware',) + MIDDLEWARE
     CORS_ORIGIN_ALLOW_ALL = True
     CORS_ALLOW_CREDENTIALS = True
