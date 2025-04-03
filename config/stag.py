c482a16a52920cfc39713bc4392383c6c7c8702d
xogas <askxogas@gmail.com>
2025-04-03 11:35:33 +0800

    添加数据库

--- config/stag.py
+++ config/stag.py
@@ -10,16 +10,17 @@ Unless required by applicable law or agreed to in writing, software distributed 
 an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
+
 from config import RUN_VER
 from config.default import FRONTEND_BACKEND_SEPARATION
 
-if RUN_VER == "open":
+if RUN_VER == 'open':
     from blueapps.patch.settings_open_saas import *  # noqa
 else:
     from blueapps.patch.settings_paas_services import *  # noqa
 
 # 预发布环境
-RUN_MODE = "STAGING"
+RUN_MODE = 'STAGING'
 
 # 只对预发布环境日志级别进行配置，可以在这里修改
 # from blueapps.conf.log import set_log_level # noqa
@@ -40,10 +41,17 @@ RUN_MODE = "STAGING"
 #     }
 # )
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',  # SQLite 引擎
+        'NAME': 'db.sqlite3',
+    }
+}
+
 # 前后端开发模式下支持跨域配置
 if FRONTEND_BACKEND_SEPARATION:
-    INSTALLED_APPS += ("corsheaders",)
+    INSTALLED_APPS += ('corsheaders',)
     # 该跨域中间件需要放在前面
-    MIDDLEWARE = ("corsheaders.middleware.CorsMiddleware",) + MIDDLEWARE
+    MIDDLEWARE = ('corsheaders.middleware.CorsMiddleware',) + MIDDLEWARE
     CORS_ORIGIN_ALLOW_ALL = True
     CORS_ALLOW_CREDENTIALS = True
