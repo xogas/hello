bcc05e9d926a4d9326e1636ccabdf5abd59f1a50
xogas <57179186+xogas@users.noreply.github.com>
2025-07-31 16:35:45 +0800

    Update stag.py

--- config/stag.py
+++ config/stag.py
@@ -31,8 +31,12 @@ RUN_MODE = 'STAGING'
 DATABASES.update(
     {
         'default': {
-            'ENGINE': 'django.db.backends.sqlite3',  # SQLite 引擎
-            'NAME': 'db.sqlite3',
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.environ.get('GCS_MYSQL_NAME'),
+            'USER': os.environ.get('GCS_MYSQL_USER'),
+            'PASSWORD': os.environ.get('GCS_MYSQL_PASSWORD'),
+            'HOST': os.environ.get('GCS_MYSQL_HOST'),
+            'PORT': os.environ.get('GCS_MYSQL_PORT'),
         }
     }
 )
